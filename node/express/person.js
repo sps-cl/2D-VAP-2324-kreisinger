eadf616d2b84f159db2ef4b56ab7618afc0bdc5d
jindrichkreisinger <jindrich.kreisinger@student.sps-cl.cz>
2024-03-05 16:01:18 +0100

    nehotovy kod

---
+++ node/express/person.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(firstName,lastName,id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+}
+
+module.exports = Person;
