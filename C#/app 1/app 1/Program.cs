568c6e85e2e95e90b44eab07354b500ef098f89e
jindrichkreisinger <jindrich.kreisinger@student.sps-cl.cz>
2024-03-26 16:50:10 +0100

    prvni C#

---
+++ C#/app 1/app 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vložte první číslo pro součet");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine($"vaše první číslo je: {a}");
+            Console.WriteLine("vložte druhé číslo pro součet");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"výsledek je: {a + b}");
+            Console.Read();
+        }
+    }
+}
