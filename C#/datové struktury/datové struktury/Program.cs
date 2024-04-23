b5788713d18c6639695dba33a1e5cd3c318919c5
Jindrichkreisinger <jindrich.kreisinger@student.sps-cl.cz>
2024-04-23 20:42:00 +0200

    dodělaný úkol s datovými strukturami

--- C#/datové struktury/datové struktury/Program.cs
+++ C#/datové struktury/datové struktury/Program.cs
@@ -16,16 +16,20 @@ namespace datové_struktury
         static void Main(string[] args)
         {
             bool kontrola = true;
-            while (kontrola) { 
+            Stack<string> posledníHodnota = new Stack<string>();
             Stack<string> stack = new Stack<string>();
-
-                Console.WriteLine("vpište příkaz, až budete chtít přestat napište: stop");
+            Console.WriteLine("vpište příkaz, až budete chtít přestat napište: stop");
+            while (kontrola) { 
                 string input = Console.ReadLine();
                 string[] separatedInput = input.Split(':');
-                string x = separatedInput[0].Trim();
-                string y = separatedInput[1].Trim();
+                if (separatedInput.Length >= 2)
+                {
+                    string x = separatedInput[0].Trim();
+                    string y = separatedInput[1].Trim();
+
                 if (x == "Pridat")
                 {
+                    Console.WriteLine(y);
                     stack.Push(y);
 
                 }
@@ -33,9 +37,29 @@ namespace datové_struktury
                 {
                     kontrola = false;
                 }
+                
+                }
+                else if (input == "Zpet")
+                {
+                    
+                    if (stack.Count > 1)
+                    {
+                        posledníHodnota.Push(stack.Pop());
+                        string Minulahodnota = stack.Peek();
+                        Console.WriteLine(Minulahodnota);
+                    }
+                    else Console.WriteLine(stack.Peek());
+                }
+                else if(input == "Vpred"){
+                    if (posledníHodnota.Count > 0) {
+                        stack.Push(posledníHodnota.Pop());
+                        Console.WriteLine(stack.Peek());
+                    }
+                    else Console.WriteLine(stack.Peek());
+
+                }
                 else Console.WriteLine("neplatný příkaz");
-                x = null;
-                y = null;
+                
                 
             }
                 
